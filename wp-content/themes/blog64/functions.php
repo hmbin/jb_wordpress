8cd02989fe9d20655ad198bf08f1af916724263a
hmbin <334777557@qq.com>
2016-06-13 16:51:49 +0800

    custom sidebar

--- wp-content/themes/blog64/functions.php
+++ wp-content/themes/blog64/functions.php
@@ -135,8 +135,8 @@ function blog64_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<div class="single">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
+		'before_title'  => '<h2 class="widget-title custom-widget-title">',
+		'after_title'   => '</h2>',
 	) );
 
 	register_sidebar( array(

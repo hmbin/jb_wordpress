0e31bb0435ea8911820828a18e5dee11d1cfb443
hmbin <334777557@qq.com>
2016-06-22 23:26:20 +0800

    contact

--- wp-content/themes/blog64/archive.php
+++ wp-content/themes/blog64/archive.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * Template Name: archive
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
--- wp-content/themes/blog64/profile.php
+++ wp-content/themes/blog64/profile.php
@@ -10,26 +10,25 @@
 <div class="company_right">
   <div class="company_content">
     <?php 
-    	$t_url = '/profile';
     	$c_url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-    	$h_url = site_url();
-    	$c_length = strlen($c_url);
-    	$h_length = strlen($h_url);
-		if($c_length > $h_length){ 
-			$t_url = substr($c_url,$h_length,$c_length); 
-		} 
 
 		global $wp_query;
 	    $cat_ID = get_query_var('cat');
 	    $category = get_category($cat_ID);
 	    $cat_slug =  $category->slug;
-	    //echo "string".$cat_slug;
-	    $html_code = do_shortcode("[include-page id='".$cat_slug."']");
-	    if($html_code){
-	    	echo do_shortcode("[include-page id='".$cat_slug."']");
+
+	    if(strpos($c_url,"/consult")){
+	    	echo do_shortcode("[include-page id='consult']");
 	    }else{
-	    	header("Location:product/"); 
+	    	//echo "aa".strpos($c_url,"/advisory");
+		    $html_code = do_shortcode("[include-page id='".$cat_slug."']");
+		    if($html_code){
+		    	echo do_shortcode("[include-page id='".$cat_slug."']");
+		    }else{
+		    	header("Location:product/"); 
+		    }
 	    }
+	    
     	
 	?>
 
